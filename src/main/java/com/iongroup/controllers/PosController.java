a6f369d89bae3fd54acde86857ec3b0393ea5f96
mirrerror <cayote20@mail.ru>
2025-12-26 18:15:27 +0200

    add pos and user controllers and views

---
+++ src/main/java/com/iongroup/controllers/PosController.java
@@ -0,0 +1,61 @@
+package com.iongroup.controllers;
+
+import com.iongroup.data.pos.connection.ConnectionType;
+import com.iongroup.service.CityService;
+import com.iongroup.service.PosService;
+import com.iongroup.service.dto.SavePosDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/pos")
+@RequiredArgsConstructor
+public class PosController {
+
+    private final PosService posService;
+    private final CityService cityService;
+
+    @GetMapping("/browse")
+    public String browse(@PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable pageable,
+                         Model model) {
+        model.addAttribute("posPage", posService.findByFilter(null, pageable));
+        return "pos/browse";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("posDto", new SavePosDto());
+        populateFormAttributes(model);
+        return "pos/add";
+    }
+
+    @PostMapping("/save")
+    public String save(@Valid @ModelAttribute("posDto") SavePosDto posDto,
+                       BindingResult bindingResult,
+                       Model model) {
+        if (bindingResult.hasErrors()) {
+            populateFormAttributes(model);
+            return "pos/add";
+        }
+        posService.create(posDto);
+        return "redirect:/pos/browse";
+    }
+
+    private void populateFormAttributes(Model model) {
+        model.addAttribute("cities", cityService.findAll(Pageable.unpaged()).getContent());
+        model.addAttribute("connectionTypes", ConnectionType.values());
+        model.addAttribute("daysOfWeek", IntStream.range(1, 8).boxed().toList());
+    }
+}
---
+++ src/main/java/com/iongroup/controllers/UserController.java
@@ -0,0 +1,58 @@
+package com.iongroup.controllers;
+
+import com.iongroup.data.user.UserType;
+import com.iongroup.service.UserService;
+import com.iongroup.service.dto.SaveUserDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@Secured("ROLE_ADMIN")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/browse")
+    public String browse(@PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable pageable,
+                         Model model) {
+        model.addAttribute("users", userService.findByFilter(null, pageable));
+        return "users/browse";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("userDto", new SaveUserDto());
+        model.addAttribute("userTypes", UserType.values());
+        return "users/add";
+    }
+
+    @PostMapping("/save")
+    public String save(@Valid @ModelAttribute("userDto") SaveUserDto userDto,
+                       BindingResult bindingResult,
+                       Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("userTypes", UserType.values());
+            return "users/add";
+        }
+
+        try {
+            userService.create(userDto);
+        } catch (IllegalArgumentException e) {
+            bindingResult.rejectValue("login", "error.login", e.getMessage());
+            model.addAttribute("userTypes", UserType.values());
+            return "users/add";
+        }
+
+        return "redirect:/users/browse";
+    }
+}
