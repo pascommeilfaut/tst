9b0c40f266af56b810e2db5e41e0cd324bb43fba
Dmitrii Bujilov <Dmitrii.Bujilov@iongroup.com>
2025-12-29 12:49:23 +0200

    move dashboard controller into the proper package, remove redundant old frontend files

---
+++ src/main/java/com/iongroup/controllers/DashboardController.java
@@ -0,0 +1,16 @@
+package com.iongroup.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+    @GetMapping
+    public String showDashboard() {
+        return "dashboard";
+    }
+
+}
