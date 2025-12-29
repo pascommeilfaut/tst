68fdecb8a1f09868537ce5d89542eebbc123bc34
mirrerror <cayote20@mail.ru>
2025-12-29 17:08:40 +0200

    fix the issue links

--- src/main/java/com/iongroup/controllers/DashboardController.java
+++ src/main/java/com/iongroup/controllers/DashboardController.java
@@ -3,8 +3,9 @@ package com.iongroup.controllers;
 import com.iongroup.data.issue.status.IssueStatus;
 import com.iongroup.service.IssueService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,15 +21,18 @@ public class DashboardController {
     private final IssueService issueService;
 
     @GetMapping
-    public String showDashboard(Model model) {
+    public String showDashboard(
+            @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            Model model) {
+
         Map<IssueStatus, Long> counts = issueService.countIssuesByStatus();
         model.addAttribute("newIssuesNumber", counts.getOrDefault(IssueStatus.NEW, 0L));
         model.addAttribute("pendingIssuesNumber", counts.getOrDefault(IssueStatus.PENDING, 0L));
         model.addAttribute("assignedIssuesNumber", counts.getOrDefault(IssueStatus.ASSIGNED, 0L));
         model.addAttribute("inProgressIssuesNumber", counts.getOrDefault(IssueStatus.IN_PROGRESS, 0L));
 
-        model.addAttribute("issues", issueService.findByFilter(null,
-                PageRequest.of(0, 20, Sort.Direction.DESC, "createdAt")));
+        model.addAttribute("issues", issueService.findByFilter(null, pageable));
+        model.addAttribute("currentUrl", "/dashboard");
 
         return "dashboard";
     }
