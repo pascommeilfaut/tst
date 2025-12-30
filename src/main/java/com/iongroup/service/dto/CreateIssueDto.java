30d436cb2c37f96331bfc2df8ec5576eb11abb48
mirrerror <cayote20@mail.ru>
2025-12-30 13:11:38 +0200

    fix spel error

--- src/main/java/com/iongroup/service/dto/CreateIssueDto.java
+++ src/main/java/com/iongroup/service/dto/CreateIssueDto.java
@@ -13,7 +13,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class CreateIssueDto {
 
-    private Integer id;
     @NotNull private Integer posId;
     @NotNull private Integer issueTypeId;
     @NotNull private Integer subTypeId;
